b4cfb61ec360e6c9c9203ffdb227dba1f9b882eb
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-09 13:32:33 +0100

    index and login page linked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const port = 3000;
@@ -9,8 +10,13 @@ const authRouter = require("./server/routes/authRoutes");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/auth", authRouter);
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/views/index.html");
+});
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/client/views/login.html");
+});
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
--- server/controllers/auth/authLoginController.js
+++ server/controllers/auth/authLoginController.js
@@ -1,5 +1,4 @@
 const { Request, Response } = require("express");
-const Prisma = require("../../database/connection.js");
 const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
