e2c4a73c02639a4e3adbfa7f6b1371db3320edd9
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-14 13:28:35 +0100

    my account page added + login page edited

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/views/index.html");
 });
 app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/client/views/login.html");
 });
+
+const login = require("./server/controllers/auth/authLoginController");
+app.post("/login",login);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- server/controllers/auth/authLoginController.js
+++ server/controllers/auth/authLoginController.js
@@ -1,8 +1,20 @@
 const { Request, Response } = require("express");
+const Prisma = require("../../connection/connection");
 const jwt = require("jsonwebtoken");
 
+
 const login = async (req, res) => {
+    const { username, password } = req.body;
+    const founded = await Prisma.utenti.findMany()
   
+    console.log(founded)
+
+    if (founded.length != 0) {
+        res.sendFile(__dirname+"../../../client/views/myaccount.html");
+    } else {
+        res.write("credenziali sbagliate");
+      
+    }
 };
 
 module.exports = login;
