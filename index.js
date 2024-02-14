df0d0658b3d764b8df200a1cc56eeb1a284009d3
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-14 12:53:38 +0100

    bozza 1

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-const authRouter = require("./server/routes/authRoutes");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use("/auth", authRouter);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/views/index.html");
 });
