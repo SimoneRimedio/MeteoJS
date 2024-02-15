464c5ac7be0da5f4cbc653b7fbfd03012b7df3bc
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-15 22:01:38 +0100

    minor changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/views/index.html");
