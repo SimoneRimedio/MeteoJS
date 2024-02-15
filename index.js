247e289834120a908f54ed03a26533094674ade5
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-15 22:26:55 +0100

    basic meteo api added + client directories removed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const authRouter = require("./server/routes/authRoutes.js");
-
 const app = express();
 const port = 3000;
+
+const authRouter = require("./server/routes/authRoutes.js");
+const weatherData = require("./server/api/meteo.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/views/index.html");
 });
 
-app.use("/auth",authRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ server/api/meteo.js
@@ -0,0 +1,46 @@
+const { fetchWeatherApi } = require('openmeteo');
+
+const params = {
+    "latitude": 52.52,
+    "longitude": 13.41,
+    "hourly": "temperature_2m",
+    "timezone": "Europe/Berlin"
+};
+const url = "https://api.open-meteo.com/v1/forecast";
+const responses = async(req,res) =>{ await fetchWeatherApi(url, params)};
+
+// Funzione ausiliaria per formare intervalli di tempo
+const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+// Processa la prima posizione. Aggiungi un ciclo for per più posizioni o modelli meteorologici
+const response = responses[0];
+
+// Attributi per fuso orario e posizione
+const utcOffsetSeconds = response.utcOffsetSeconds();
+const timezone = response.timezone();
+const timezoneAbbreviation = response.timezoneAbbreviation();
+const latitude = response.latitude();
+const longitude = response.longitude();
+
+const hourly = response.hourly();
+
+// Nota: L'ordine delle variabili meteorologiche nella query URL e gli indici sotto devono corrispondere!
+const weatherData = {
+    hourly: {
+        time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+            (t) => new Date((t + utcOffsetSeconds) * 1000)
+        ),
+        temperature2m: hourly.variables(0).valuesArray(),
+    },
+};
+
+// `weatherData` ora contiene una struttura semplice con array per data e dati meteorologici
+for (let i = 0; i < weatherData.hourly.time.length; i++) {
+    console.log(
+        weatherData.hourly.time[i].toISOString(),
+        weatherData.hourly.temperature2m[i]
+    );
+}
+
+module.exports = weatherData;
