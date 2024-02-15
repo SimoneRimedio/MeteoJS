8dc5c8e348b4272ff2bd04da8425a454265f36e1
SimoneRimedio <rimediosimone1@gmail.com>
2024-02-15 21:57:38 +0100

    minor changes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const authRouter = require("./server/routes/authRoutes.js");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/views/index.html");
